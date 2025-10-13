92a3a3b38b7eafb1e7f26add985cdd072d72d30f
minq3010 <nmquoc204@gmail.com>
2025-10-13 16:56:15 +0700

    refactor: update Image struct and ImageRequest for improved clarity and consistency

--- pkg/models/image.go
+++ pkg/models/image.go
@@ -3,9 +3,10 @@ package models
 // Image represents an image file entity
 type Image struct {
 	ID       string  `gorm:"primaryKey;size:12" json:"id"`
-	DialogID string  `gorm:"size:12;index;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"dialog_id"`
-	PromptID *string `gorm:"size:12;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"prompt_id"`
-	FileURL  string  `json:"file_url"`
+	DialogID *string `gorm:"size:12;index;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"dialog_id"`
+	TopicID  *string `gorm:"size:12;index;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"topic_id"`
+	FileURL  string  `json:"file_url" gorm:"type:text; not null"`
+	IsFigure bool    `json:"is_figure" gorm:"type:boolean; not null;default:false"`
 }
 
 // TableName overrides the table name used by Image to `images`
@@ -17,10 +18,7 @@ type ImageFigureRequest struct {
 	Infor_1 string `json:"infor_1"`
 	Infor_2 string `json:"infor_2"`
 }
-
 type ImageRequest struct {
-	TopicID  string `json:"topic_id"`
-	DialogID string `json:"dialog_id"`
-	Infor_1  string `json:"infor_1"`
-	Infor_2  string `json:"infor_2"`
+	PathFigure string `json:"path_figure"`
 }
+
